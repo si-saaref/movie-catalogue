37701c3dfd227c6a69afd9db87685a86cc5e5165
si-saaref <achmadsyarif241@gmail.com>
2023-06-21 20:42:01 +0700

    feat: implement workbox

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,11 +1,23 @@
+import * as WorkboxWindow from 'workbox-window';
+
 const swRegister = async () => {
 	if (!('serviceWorker' in navigator)) {
 		console.log('Service Worker not supported in the browser');
 		return;
 	}
 
+	// Todo => without workbox
+	// try {
+	// 	await navigator.serviceWorker.register('./sw.bundle.js');
+	// 	console.log('Service worker registered');
+	// } catch (error) {
+	// 	console.log('Failed to register service worker', error);
+	// }
+
+	// Todo => with workbox
+	const wb = new WorkboxWindow.Workbox('./sw.bundle.js');
 	try {
-		await navigator.serviceWorker.register('./sw.bundle.js');
+		await wb.register();
 		console.log('Service worker registered');
 	} catch (error) {
 		console.log('Failed to register service worker', error);
--- webpack.common.js
+++ webpack.common.js
@@ -2,10 +2,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+
 module.exports = {
 	entry: {
 		app: path.resolve(__dirname, 'src/scripts/index.js'),
-		sw: path.resolve(__dirname, 'src/scripts/sw.js'),
+		// sw: path.resolve(__dirname, 'src/scripts/sw.js'), // ! While working with workbox, this initiation no longer needed
 	},
 	output: {
 		filename: '[name].bundle.js',
@@ -39,6 +41,9 @@ module.exports = {
 					to: path.resolve(__dirname, 'dist/'),
 				},
 			],
+		}),
+		new WorkboxWebpackPlugin.GenerateSW({
+			swDest: './sw.bundle.js',
 		}),
 	],
 };
