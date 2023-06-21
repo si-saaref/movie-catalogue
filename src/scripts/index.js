3cc2406a2deac658ff932277fc30b3b5a2d97c03
si-saaref <achmadsyarif241@gmail.com>
2023-06-21 15:07:40 +0700

    feat: registrasi service worker

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,7 @@ import '../styles/style.css';
 import '../styles/responsive.css';
 
 import App from './views/app';
+import swRegister from './utils/sw-register';
 
 const app = new App({
 	button: document.querySelector('#hamburgerButton'),
@@ -18,4 +19,5 @@ window.addEventListener('hashchange', () => {
 
 window.addEventListener('load', () => {
 	app.renderPage();
+	swRegister();
 });
---
+++ src/scripts/sw.js
@@ -0,0 +1,27 @@
+// ! Siklus Pertama
+self.addEventListener('install', (event) => {
+	// ? self.skipWaiting() //! optional in dev mode
+	console.log('Installing Service Worker ...');
+
+	// TODO: Caching App Shell Resource
+});
+
+// ! Siklus Kedua
+self.addEventListener('activate', (event) => {
+	console.log('Activating Service Worker ...');
+
+	// TODO: Delete old caches
+});
+
+// ! Siklus ketiga (banyak macamnya)
+self.addEventListener('fetch', (event) => {
+	console.log(event.request);
+
+	event.respondWith(fetch(event.request));
+	// TODO: Add/get fetch request to/from caches
+});
+
+/* 
+! Siklus hidup terbagi menjadi 3 = Installation, Activation, Idle (fetch, push, sync)
+
+*/
