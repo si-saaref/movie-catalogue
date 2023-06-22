dd955d5a287f8be133d75dbc2cadfe908c9500d0
si-saaref <achmadsyarif241@gmail.com>
2023-06-22 16:43:30 +0700

    feat: add websocket config

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,8 @@ import '../styles/responsive.css';
 
 import App from './views/app';
 import swRegister from './utils/sw-register';
+import WebSocketInitiator from './utils/websocket-initiator';
+import CONFIG from './globals/config';
 
 const app = new App({
 	button: document.querySelector('#hamburgerButton'),
@@ -20,4 +22,5 @@ window.addEventListener('hashchange', () => {
 window.addEventListener('load', () => {
 	app.renderPage();
 	swRegister();
+	WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
 });
